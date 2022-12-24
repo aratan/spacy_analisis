a6fd1d6edb794ad400edb6dc2b326fe9f34300ca
Victor Arbiol <systemdeveloper868@gmail.com>
2022-12-24 13:58:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,26 +11,28 @@ doc = nlp("Fuck Apple es una empresa de tecnología con sede en Cupertino, Calif
 
 ## xxxxxxx
 
-print(doc._.blob.polarity)
+print(f"sentimientos ", doc._.blob.polarity)
 # -0.125
 
-print(doc._.blob.subjectivity)
+print(f"subjetivo ",doc._.blob.subjectivity)
 # 0.9
 
 print(doc._.blob.sentiment_assessments.assessments)
 
 # Eliminar signos de puntuación y convertir todas las palabras a minúsculas
 tokens = [token.text.lower() for token in doc if not token.is_punct]
-print(tokens)
+print(f"\n{tokens}")
 
 # Tokenizar el texto en palabras individuales
 tokens = [token for token in doc]
-print(tokens)
+print(f"\n{tokens}")
 
 # Identificar entidades en el texto
 entities = [(entity.text, entity.label_) for entity in doc.ents]
+print("\nEntidades")
 print(entities)
 
 # Identificar temas en el texto
 topics = [(token.text, token.lemma_) for token in doc if token.pos_ == "NOUN"]
+print("\nTEMAS:")
 print(topics)
